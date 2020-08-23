8e5e032cff4555316c5424e84ea4e20ae82d3498
diogobarrel <santos.barrel@gmail.com>
2020-08-22 22:29:46 -0300

    docstring

--- data/data.py
+++ data/data.py
@@ -9,16 +9,23 @@ import numpy as np
 
 from audio.audio import AudioFile
 from util.host import URBANSOUND_8K_AUDIO, URBANSOUND_8K_METADATA
+from util.utils import timeit
 
-
+@timeit
 def build_features_dataframe():
+    """
+    uses metadata info to load all dataset files and build
+    a pandas dataframe of those audio features
+    """
     features = []
     # Iterate through each sound file and extract the features
     metadata = pd.read_csv(URBANSOUND_8K_METADATA)
     for _, row in metadata.iterrows():
 
-        file_name = os.path.join(os.path.abspath(
-            URBANSOUND_8K_AUDIO), 'fold'+str(row["fold"])+'/', str(row["slice_file_name"]))
+        file_name = os.path.join(
+            os.path.abspath(URBANSOUND_8K_AUDIO),
+            'fold'+str(row["fold"])+'/',
+            str(row["slice_file_name"]))
 
         class_label = row["class"]
         print(file_name, class_label)
