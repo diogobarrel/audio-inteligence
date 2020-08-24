c8abc8a69865d11d64728420b149599ef2a78f08
diogobarrel <diogo.barrel@stilingue.com.br>
2020-08-24 11:22:14 -0300

    add mfcc plot

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ ROW, COL = 1, 1
 for sample in audio_samples:
     # audio_file = AudioFile(sample)
     # print(audio_file.extract_mfcc_features())
-    signal.spectogram(sample, index=f'{ROW}{COL}')
+    signal.draw_mfcc(sample)
     # signal.stft_spectogram(sample, index=f'{ROW+1}{COL}')
     # signal.draw_spec3(sample, index=f'{ROW+2}{COL}')
     COL += 1
